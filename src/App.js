b55156d3c0b5fb24709c6a84818a51a1d9dd869b
Esdras Pavon <pavonesdras@gmail.com>
2019-04-09 11:30:37 -0300

    refs added in form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from "./components/Header";
 import Form from "./components/Form";
 
 class App extends Component {
+  makeDate = () => {};
   render() {
     return (
       <div className="container">
         <Header title={"Administrador de pacientes de veterinaria"} />
         <div className="row">
           <div className="col-md-6">
-            <Form />
+            <Form makeDate={this.makeDate} />
           </div>
         </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  //refs
+  nameRef = React.createRef();
+  ownRef = React.createRef();
+  dateRef = React.createRef();
+  timeRef = React.createRef();
+  symptomRef = React.createRef();
+
   state = {};
+
+  makeNewDate = e => {
+    e.preventDefault();
+
+    console.log(this.nameRef.current.value);
+    console.log(this.ownRef.current.value);
+    console.log(this.dateRef.current.value);
+    console.log(this.timeRef.current.value);
+    console.log(this.symptomRef.current.value);
+    this.props.makeDate();
+  };
   render() {
     return (
       <div className="card mt-5">
         <div className="card-body">
           <h2 className="card-title text-center mb-5">Agrega las citas aqui</h2>
-          <form>
+          <form onSubmit={this.makeNewDate}>
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">
                 Nombre Mascota
@@ -17,6 +35,7 @@ class Form extends Component {
                   type="text"
                   className="form-control"
                   placeholder="Nombre Mascota"
+                  ref={this.nameRef}
                 />
               </div>
             </div>
@@ -29,6 +48,7 @@ class Form extends Component {
                   type="text"
                   className="form-control"
                   placeholder="Nombre Dueño de la Mascota"
+                  ref={this.ownRef}
                 />
               </div>
             </div>
@@ -36,12 +56,20 @@ class Form extends Component {
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
               <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
-                <input type="date" className="form-control" />
+                <input
+                  type="date"
+                  className="form-control"
+                  ref={this.dateRef}
+                />
               </div>
 
               <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
               <div className="col-sm-8 col-lg-4">
-                <input type="time" className="form-control" />
+                <input
+                  type="time"
+                  className="form-control"
+                  ref={this.timeRef}
+                />
               </div>
             </div>
 
@@ -50,7 +78,7 @@ class Form extends Component {
                 Sintomas
               </label>
               <div className="col-sm-8 col-lg-10">
-                <textarea className="form-control" />
+                <textarea className="form-control" ref={this.symptomRef} />
               </div>
             </div>
             <div className="form-group row justify-content-end">
